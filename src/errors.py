070fdc66604ae9abd330512b8f738e776d61f524
tony-cypher <tonycypher0@gmail.com>
2025-09-02 10:10:39 +0100

    feat: InvalidToken error handler

---
+++ src/errors.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, status
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
+from typing import Any, Callable
+
+
+class EcommerceException(Exception):
+    """This is the base class for all E-commerce errors"""
+
+    pass
+
+
+class InvalidToken(EcommerceException):
+    """User has provided an invalid or expired token"""
+
+    pass
+
+
+def create_exception_handler(
+    status_code: int, initial_detail: Any
+) -> Callable[[Request, Exception], JSONResponse]:
+    async def exception_handler(request: Request, exec: EcommerceException):
+        return JSONResponse(content=initial_detail, status_code=status_code)
+
+    return exception_handler
+
+
+def register_all_errors(app: FastAPI):
+    app.add_exception_handler(
+        InvalidToken,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message": "Token is invalid or expired",
+                "resolution": "Please get new token",
+                "error_code": "Invalid token",
+            },
+        ),
+    )
